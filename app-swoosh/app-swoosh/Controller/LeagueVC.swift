f6f4a26bc34979a8e978c74a2dd530945e80345b
alpmehmetozturk <alpmehmetozturk@gmail.com>
2021-11-17 06:41:18 +0300

    secme butonlari yapildi

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var nextButton: BorderButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
     
@@ -20,6 +22,22 @@ class LeagueVC: UIViewController {
             performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
 
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+        
+        selectLeague(leagueType: "womens")
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+
+    func selectLeague(leagueType:String){
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
+   
+    }
     /*
     // MARK: - Navigation
 
